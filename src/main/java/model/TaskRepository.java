192fc33d05366f4e41555dfd149ab2226d72e6cf
Alexs Ruiz <ralexs.acu@gmail.com>
2025-08-01 23:29:51 -0600

    Create the Task Repository

---
+++ src/main/java/model/TaskRepository.java
@@ -0,0 +1,70 @@
+package model;
+
+import exceptions.TaskException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRepository {
+    List<Task> tasks = new ArrayList<>();
+
+    public void save(Task task) throws TaskException {
+        if(task == null) {
+            throw new TaskException("Task could not be null");
+        }
+        tasks.add(task);
+    }
+
+    public Task findById(String id){
+        for(Task task: tasks) {
+            if(task.getId().equals(id)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    public void remove(String id) throws TaskException {
+        Task task = findById(id);
+        if(task == null) {
+            throw new TaskException("Task could not be null");
+        }
+        tasks.remove(task);
+    }
+    public void remove(Task task) throws TaskException {
+        if(task == null) {
+            throw new TaskException("Task could not be null");
+        }
+        if(!tasks.contains(task)) {
+            throw new TaskException("Task does not exist in the list");
+        }
+        tasks.remove(task);
+    }
+
+    public List<Task> findAll() throws TaskException {
+        if(tasks.isEmpty()) {
+            throw new TaskException("List is empty");
+        }
+        return tasks;
+    }
+
+    public int findIndexById(String id){
+        for (int i = 0; i < tasks.size(); i++) {
+            if(tasks.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void updateTask(Task updatedTask) throws TaskException {
+        if(updatedTask == null) {
+            throw new TaskException("Task could not be null");
+        }
+        int index = findIndexById(updatedTask.getId());
+        if(index == -1) {
+            throw new TaskException("Invalid index");
+        }
+        tasks.set(index, updatedTask);
+    }
+}
