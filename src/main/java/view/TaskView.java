a7edc2c9f930680989d2159527f9a082aa140373
Alexs Ruiz <ralexs.acu@gmail.com>
2025-08-24 17:40:21 -0600

    Add methods to handle the crud operations of the task

--- src/main/java/view/TaskView.java
+++ src/main/java/view/TaskView.java
@@ -3,6 +3,7 @@ package view;
 import controller.TaskController;
 import exceptions.TaskException;
 import exceptions.TaskValidationException;
+import model.Task;
 
 import java.util.Scanner;
 
@@ -10,9 +11,9 @@ public class TaskView {
     private final TaskController taskController;
     private final Scanner scanner;
 
-    public TaskView(TaskController taskController, Scanner scanner) {
+    public TaskView(TaskController taskController) {
         this.taskController = taskController;
-        this.scanner = scanner;
+        this.scanner = new Scanner(System.in);
     }
 
     public void showMenu(){
@@ -21,7 +22,7 @@ public class TaskView {
             System.out.println("1. Add task");
             System.out.println("2. Delete task");
             System.out.println("3. Update task");
-            System.out.println("4. Remove task");
+            System.out.println("4. Show task");
             System.out.println("5. Exit");
 
             String option = scanner.nextLine();
@@ -30,37 +31,124 @@ public class TaskView {
                     addTaskView();
                     break;
                 case "2":
+                    removeTaskView();
                     break;
                 case "3":
+                    updateTaskView();
                     break;
                 case "4":
+                    showTaskView();
                     break;
                 case "5":
-                    break;
+                    System.out.println("Finishing the program execution...");
+                    return;
+                default:
+                    System.out.println("Invalid option. Try again.");
             }
         }
     }
 
     public void addTaskView() {
         try {
-            System.out.println("Insert ID: ");
-            String id = scanner.nextLine();
+            var task = this.getTaskInput();
 
-            System.out.println("Insert the title");
-            String title = scanner.nextLine();
-
-            System.out.println("Insert the description");
-            String description = scanner.nextLine();
-
-            System.out.println("Is the task completed? true/false");
-            Boolean completed = Boolean.parseBoolean(scanner.nextLine());
-
-            taskController.addTask(id, title, description, completed);
+            taskController.addTask(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted());
+            System.out.println("Task created");
         } catch (TaskValidationException | TaskException e) {
             System.out.println("Error: " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Unexpected error");
             e.printStackTrace();
         }
+    }
+
+    public void removeTaskView() {
+        try {
+            System.out.println("Insert the id to remove");
+            String id = scanner.nextLine();
+            this.taskController.removeTask(id);
+
+            System.out.println("Task deleted successfully");
+        } catch (TaskValidationException | TaskException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Unexpected error");
+            e.printStackTrace();
+        }
+    }
+
+    public void showTaskView() {
+        try {
+            this.taskController.showTasks();
+        } catch (TaskValidationException | TaskException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Unexpected error");
+            e.printStackTrace();
+        }
+    }
+
+    public void updateTaskView() {
+        try {
+            var task = this.getTaskInput();
+
+            taskController.updateTask(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted());
+            System.out.println("Task updated");
+        } catch (TaskValidationException | TaskException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Unexpected error");
+            e.printStackTrace();
+        }
+    }
+
+    private Task getTaskInput() {
+        String id, title, description;
+        Boolean completed = null;
+
+        do {
+            System.out.println("Insert ID: ");
+            id = scanner.nextLine();
+
+            if(id.isEmpty()) {
+                System.out.println("id cannot be empty");
+            }
+        } while (id.isEmpty());
+
+        do {
+            System.out.println("Insert the title");
+            title = scanner.nextLine();
+
+            if(title.isEmpty()) {
+                System.out.println("title cannot be empty");
+            }
+        } while (title.isEmpty());
+
+        do {
+            System.out.println("Insert the description");
+            description = scanner.nextLine();
+
+            if(description.isEmpty()) {
+                System.out.println("description cannot be empty");
+            }
+        } while (description.isEmpty());
+
+
+        while (completed == null) {
+            System.out.println("Is the task completed? true/false");
+            String input = scanner.nextLine().trim().toLowerCase();
+
+            if(input.equals("true")) {
+                completed = true;
+            }
+
+            if(input.equals("false")) {
+                completed = false;
+            }
+
+            System.out.println("Please enter a valid option: true or false");
+        };
+
+        return new Task(id, title, description, completed);
     }
 }
